70f35307fb0c7800e0ece76b207c903e49ab35c4
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-11-26 14:18:22 +0700

    feat: add Swagger documentation and user registration endpoint with file upload support

--- day04_crud/controllers/user_controller.go
+++ day04_crud/controllers/user_controller.go
@@ -10,6 +10,27 @@ import (
 
 var db *sql.DB
 
+func InitDB(database *sql.DB) {
+	db = database
+}
+
+// Register handles the user registration process.
+// @Summary Register a new user
+// @Description This endpoint registers a new user by accepting their name, username, password, and photo.
+// @Tags users
+// @Accept multipart/form-data
+// @Produce application/json
+// @Param name formData string true "Name of the user"
+// @Param username formData string true "Username of the user"
+// @Param password formData string true "Password of the user"
+// @Param photo formData file true "Photo of the user"
+// @Success 200 {object} map[string]string{"message": "Berhasil mendaftar"}
+// @Failure 400 {object} map[string]string{"error": "Gagal membaca file foto"}
+// @Failure 400 {object} map[string]string{"error": "Semua form harus diisi"}
+// @Failure 400 {object} map[string]string{"error": "Gagal menyimpan file foto"}
+// @Failure 500 {object} map[string]string{"error": "Gagal mengenkripsi password"}
+// @Failure 500 {object} map[string]string{"error": "Gagal menyimpan user"}
+// @Router /register [post]
 func Register(c *gin.Context) {
 	name := c.PostForm("name")
 	username := c.PostForm("username")
--- day04_crud/main.go
+++ day04_crud/main.go
@@ -6,11 +6,30 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/ruhyadi/multimatics/day04_crud/controllers"
 	"github.com/ruhyadi/multimatics/day04_crud/db"
+	_ "github.com/ruhyadi/multimatics/day04_crud/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title Simple CRUD API
+// @version 1.0
+// @description This is a simple CRUD API with Gin, JWT, and MySQL.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8081
+// @BasePath /
 
 func main() {
 	log.Println("Simple CRUD API with GIN")
 	db.InitDB()
+	controllers.InitDB(db.DB)
 
 	r := gin.Default()
 
@@ -20,5 +39,8 @@ func main() {
 	// route
 	r.POST("/register", controllers.Register)
 
-	r.Run(":8080")
+	// swagger route
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(":8081")
 }
