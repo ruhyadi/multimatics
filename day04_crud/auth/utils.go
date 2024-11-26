203725e8a805a5dcfb1b059e35124ee546c9ef9d
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-11-26 15:25:30 +0700

    feat: add authentication middleware to verify JWT tokens in requests

--- day04_crud/auth/utils.go
+++ day04_crud/auth/utils.go
@@ -1,9 +1,12 @@
 package auth
 
 import (
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
 var jwtKey = []byte("didiganteng")
@@ -41,4 +44,26 @@ func VerifyToken(tokenString string) (*Claims, error) {
 	}
 
 	return claims, nil
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing or invalid token"})
+			c.Abort()
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		claims, err := VerifyToken(tokenStr)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("username", claims.Username)
+		c.Next()
+	}
 }
