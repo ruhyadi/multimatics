b0679f00765652fc6d7ab9fd4e301af56279d6d1
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-12-03 10:07:18 +0700

    feat: implement Book and Category models with migrations and controllers

--- laravel001/database/migrations/2024_12_03_024356_create_books_table.php
+++ laravel001/database/migrations/2024_12_03_024356_create_books_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone', 12);
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('category_id');
+            $table->string('title');
+            $table->string('stock');
+            $table->date('borrow_date');
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 };
