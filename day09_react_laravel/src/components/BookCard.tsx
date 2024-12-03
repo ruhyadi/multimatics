48b411b8d3d09e6bb3415741e44f1bc3be0e875e
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-12-03 15:12:14 +0700

    feat: add bookcard to homescreen

--- day09_react_laravel/src/components/BookCard.tsx
+++ day09_react_laravel/src/components/BookCard.tsx
@@ -1,25 +1,35 @@
 import React from "react";
 
-type BookCardProps = {
-    title: string;
-    category: string;
-    stock: number;
-    borrowedDate: string;
-    picture: string;
-}
+export type BookCardProps = {
+  title: string;
+  category: string;
+  stock: number;
+  borrowedDate: string;
+  picture: string;
+};
 
-const BookCard: React.FC<BookCardProps> = ({ title, category, stock, borrowedDate, picture }) => {
-    return (
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img src={picture} alt={title} className="w-full h-64 object-cover object-center" />
-            <div className="p-4">
-                <h2 className="text-lg font-bold">{title}</h2>
-                <p className="text-sm text-gray-600">{category}</p>
-                <p className="text-sm text-gray-600">Stock: {stock}</p>
-                <p className="text-sm text-gray-600">Borrowed Date: {borrowedDate}</p>
-            </div>
-        </div>
-    );
+const BookCard: React.FC<BookCardProps> = ({
+  title,
+  category,
+  stock,
+  borrowedDate,
+  picture,
+}) => {
+  return (
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:border-blue-500 border-2">
+      <img
+        src={picture}
+        alt={title}
+        className="w-full h-64 object-cover object-center"
+      />
+      <div className="p-4">
+        <h2 className="text-lg font-bold">{title}</h2>
+        <p className="text-sm text-gray-600">{category}</p>
+        <p className="text-sm text-gray-600">Stock: {stock}</p>
+        <p className="text-sm text-gray-600">Borrowed Date: {borrowedDate}</p>
+      </div>
+    </div>
+  );
 };
 
 export default BookCard;
--- day09_react_laravel/src/pages/Home.tsx
+++ day09_react_laravel/src/pages/Home.tsx
@@ -1,7 +1,61 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BookCardProps } from "../components/BookCard";
+import Swal from "sweetalert2";
+import BookCard from "../components/BookCard";
+
 const HomeScreen = () => {
+  const [isLoading, setIsloading] = useState(true);
+  const [books, setBooks] = useState<BookCardProps[]>([]);
+  const BASE_URL = "http://localhost:8000/api";
+  const TOKEN = "45|QH8y8IUteop2lQhCcbO8L6DHRTrwZ2KDjIG7FNIjc50285d8";
+
+  const loadBooks = async () => {
+    setIsloading(true);
+    axios
+      .get(`${BASE_URL}/books`, {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      })
+      .then((response) => {
+        const data = response.data.data;
+        setBooks(
+          data.map((book: any) => ({
+            title: book.title,
+            category: book.category.name,
+            stock: book.stock,
+            borrowedDate: book.borrow_date,
+            picture: book.image,
+          }))
+        );
+      })
+      .catch((error) => {
+        Swal.fire("Error", error.message, "error");
+      })
+      .finally(() => setIsloading(false));
+  };
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container mx-auto grid grid-cols-4 gap-4">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        books.map((book, index) => (
+          <BookCard
+            key={index}
+            title={book.title}
+            category={book.category}
+            stock={book.stock}
+            borrowedDate={book.borrowedDate}
+            picture={book.picture}
+          />
+        ))
+      )}
     </div>
   );
 };
